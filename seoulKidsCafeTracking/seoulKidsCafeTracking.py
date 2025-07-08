47720370c6a3aa808f9d040f2c997f90e4106d9f
shong <shongtruth@gmail.com>
2025-07-08 16:52:35 +0900

    서울형키즈카페 취소건 발생 트래킹

---
+++ seoulKidsCafeTracking/seoulKidsCafeTracking.py
@@ -0,0 +1,72 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+from win10toast import ToastNotifier
+from random import randint
+
+# 알림 호출 기본 세팅
+def bubbleSort(array):
+    n = len(array)
+    for i in range(n):
+        already_sorted = True
+
+        for j in range(n - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                already_sorted = False
+
+        if already_sorted:
+            break
+
+    return array
+
+toaster = ToastNotifier()
+array = [randint(-3000, 3000) for i in range(3000)]
+
+options = webdriver.ChromeOptions()
+# options.add_argument('headless')
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+idx = 1
+
+driver.get("https://icare.seoul.go.kr")
+
+input("로그인 후 작업 시작")
+
+while True:
+  
+  try:
+    driver.get("https://icare.seoul.go.kr/icare/user/kidsCafeResve/BD_selectKidsCafeResveCal.do?q_fcltyId=SB240702")
+    
+    time.sleep(2)
+
+    
+    firstTime = driver.find_element(By.CSS_SELECTOR, '#calendar > tbody > tr:nth-child(3) > td:nth-child(7) > div > p:nth-child(1) > span > i').text
+    secondTime = driver.find_element(By.CSS_SELECTOR, '#calendar > tbody > tr:nth-child(3) > td:nth-child(7) > div > p:nth-child(2) > span > i').text
+    thirdTime = driver.find_element(By.CSS_SELECTOR, '#calendar > tbody > tr:nth-child(3) > td:nth-child(7) > div > p:nth-child(3) > span > i').text
+    firstTime2 = driver.find_element(By.CSS_SELECTOR, '#calendar > tbody > tr:nth-child(4) > td.day > div > p:nth-child(1) > span > i').text
+    secondTime2 = driver.find_element(By.CSS_SELECTOR, '#calendar > tbody > tr:nth-child(4) > td.day > div > p:nth-child(2) > span > i').text
+    thirdTime2 = driver.find_element(By.CSS_SELECTOR, '#calendar > tbody > tr:nth-child(4) > td.day > div > p:nth-child(3) > span > i').text
+
+    print(firstTime)
+    print(secondTime)
+    print(thirdTime)
+    print(firstTime2)
+    print(secondTime2)
+    print(thirdTime2)
+
+    if firstTime == '1' or firstTime2 == '1' or secondTime == '1' or secondTime2 == '1' or thirdTime == '1' or thirdTime2 == '1':
+      toaster.show_toast("CHECK!","SEOUL CHECK", icon_path=None, duration=1000, threaded=True)
+      print("취소건 발생!(체크)")
+    else:
+      print("{}회차 탐색 중({})".format(idx, time.strftime("%Y-%m-%d %H:%M:%S")))  
+          
+    time.sleep(1)
+    toaster.__init__()
+    idx += 1
+  except Exception as e:
+    print(e)
